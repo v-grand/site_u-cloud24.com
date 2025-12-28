288d55945f438e22df5ab056d556825d8856105f
Administrator <gelonooz.invest@gmail.com>
2025-12-28 20:54:12 +0000

    debugs

--- index.tsx
+++ index.tsx
@@ -2,6 +2,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
+import './style.css'; // Import the main CSS file
 
 const rootElement = document.getElementById('root');
 if (rootElement) {
